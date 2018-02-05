1f21e628a8c33cf9b6fa7ebdc7bb26a13586fa2f
Lisa Lee <snickers495@gmail.com>
2018-02-05 20:45:15 +0000

    automatically backed up by learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -8,7 +8,8 @@ export default class YouTubeDebugger extends React.Component {
       errors: [],
       user: null,
       settings: { bitrate: 8,
-                  video: { resolution: '1080p' } }
+                  video:
+                  { resolution: '1080p' } }
     }
   }
 
