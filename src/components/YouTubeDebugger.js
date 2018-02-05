63f4a32ca1edf98be6a26062b6fb56b1f3eea49a
Lisa Lee <snickers495@gmail.com>
2018-02-05 20:50:28 +0000

    automatically backed up by learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -22,7 +22,7 @@ export default class YouTubeDebugger extends React.Component {
     this.setState({
       settings: {...this.state.settings,
                 video: {
-                  ...this.se
+                  ...this.state.settings.video, 
                 }}
     })
   }
