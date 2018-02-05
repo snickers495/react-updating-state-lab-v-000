78825fdfddc2a4ff7a79a4172505ce0f274fa1ae
Lisa Lee <snickers495@gmail.com>
2018-02-05 20:45:00 +0000

    automatically backed up by learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -5,7 +5,9 @@ export default class YouTubeDebugger extends React.Component {
   constructor() {
     super();
     this.state = {
-      errors: [], user: null, settings: { bitrate: 8, video: { resolution: '1080p' } }
+      errors: [],
+      user: null,
+      settings: { bitrate: 8, video: { resolution: '1080p' } }
     }
   }
 
