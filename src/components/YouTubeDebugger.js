d651834ad0b14ab7fba64718120984e5bbe1a747
Lisa Lee <snickers495@gmail.com>
2018-02-05 20:45:56 +0000

    automatically backed up by learn

--- src/components/YouTubeDebugger.js
+++ src/components/YouTubeDebugger.js
@@ -17,6 +17,7 @@ export default class YouTubeDebugger extends React.Component {
   }
   render(){
     return(
+      <div>
       <button className='bitrate' onClick={this.handleClick}>Bitrate</button>
     )
   }
